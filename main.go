173cad075fb743d6f7f3f417b2682e66bfa4b986
Stephan Ziegler <stephan.ziegler@sixt.com>
2017-06-02 14:47:41 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	micro "github.com/micro/go-micro"
+	proto "github.com/stevie0105/micro-example/proto"
+	"context"
+)
+
+type Greeter struct{}
+
+func (g *Greeter) Hello(ctx context.Context, req *proto.HelloRequest, rsp *proto.HelloResponse) error {
+	rsp.Greeting = "Hello " + req.Name
+	return nil
+}
+
+func main() {
+	// Create a new service. Optionally include some options here.
+	service := micro.NewService(
+		micro.Name("greeter"),
+		micro.Version("latest"),
+		micro.Metadata(map[string]string{
+			"type": "helloworld",
+		}),
+	)
+
+	// Init will parse the command line flags. Any flags set will
+	// override the above settings. Options defined here will
+	// override anything set on the command line.
+	service.Init()
+
+	// Register handler
+	proto.RegisterGreeterHandler(service.Server(), new(Greeter))
+
+	// Run the server
+	if err := service.Run(); err != nil {
+		fmt.Println(err)
+	}
+}
